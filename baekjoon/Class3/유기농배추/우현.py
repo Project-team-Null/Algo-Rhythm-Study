5b4adb1eab5d01b078215442a6c19afb12352625
Woohye0n <wh761@g.skku.edu>
2022-01-10 22:23:52 +0900

    [Class 3] 유기농배추

---
+++ baekjoon/Class3/유기농배추/우현.py
@@ -0,0 +1,30 @@
+def bfs(x, y):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
+    queue = [[x, y]]
+    while queue:
+        a, b = queue[0][0], queue[0][1]
+        del queue[0]
+        for i in range(4):
+            q = a + dx[i]
+            w = b + dy[i]
+            if 0 <= q < n and 0 <= w < m and board[q][w] == 1:
+                board[q][w] = 0
+                queue.append([q, w])
+                
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        m, n, k = map(int, input().split())
+        board = [[0] * m for i in range(n)]
+        cnt = 0
+        for j in range(k):
+            a, b = map(int, input().split())
+            board[b][a] = 1
+        for q in range(n):
+            for w in range(m):
+                if board[q][w] == 1:
+                    bfs(q, w)
+                    board[q][w] = 0
+                    cnt += 1
+        print(cnt)
