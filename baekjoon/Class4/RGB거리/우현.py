183e942213bbeadd9d5a768e1e3684838d131100
Woohye0n <wh761@g.skku.edu>
2022-01-13 17:23:47 +0900

    [Class4] RGB거리

--- baekjoon/Class4/RGB거리/우현.py
+++ baekjoon/Class4/RGB거리/우현.py
@@ -1,11 +1,11 @@
 def solution(rgb, n):
-    dp = [[0, 0, 0] for _ in range(n + 1)]
-    dp[1] = [rgb[0][0], rgb[0][1], rgb[0][2]]
+    dp = [0]
+    dp.append(rgb[0])
     for i in range(2, n + 1):
         r = rgb[i - 1][0] + min(dp[i - 1][1], dp[i - 1][2])
         g = rgb[i - 1][1] + min(dp[i - 1][0], dp[i - 1][2])
         b = rgb[i - 1][2] + min(dp[i - 1][0], dp[i - 1][1])
-        dp[i] = [r, g, b]
+        dp.append((r, g, b))
     return min(dp[n])
 
 if __name__ == '__main__':
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     rgb = []
     
     for i in range(n):
-        rgb.append(list(map(int, input().split())))
+        rgb.append(tuple(map(int, input().split())))
         
     print(solution(rgb, n))
