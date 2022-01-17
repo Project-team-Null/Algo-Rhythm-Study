2935f333d46c26545054a39387ef517979135962
Woohye0n <wh761@g.skku.edu>
2022-01-17 21:20:55 +0900

    [Class4] N-QUEEN

--- baekjoon/Class4/N-QUEEN/우현.py
+++ baekjoon/Class4/N-QUEEN/우현.py
@@ -17,19 +17,19 @@ def solution(n, col=0, last=list()):
         return 1
     
     for row in board:
-        temp_board = board[:]
-        temp_board.remove(row)
         temp_last = last[:]
         temp_last.append(row)
         cnt += solution(n, col + 1, temp_last)
     
     return cnt
 
+
 if __name__ == '__main__':
     n = int(input())
     if n == 1:
         print(1)
     else:
-        val = 2 *solution(n)
-        if n % 2: val += solution(n, 1, [int(n/2)])
+        val = 2 * solution(n)
+        if n % 2 == 1:
+            val += solution(n, 1, [int(n/2)])
         print(val)
