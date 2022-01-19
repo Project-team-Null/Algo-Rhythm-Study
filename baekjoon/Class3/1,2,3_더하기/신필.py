e26d8a27d3df03c05088a3fbf47cd2dc53570dec
lee-3-8 <leesinji8@gmail.com>
2022-01-19 13:22:15 +0900

    [Class 3]1,2,3_더하기

---
+++ baekjoon/Class3/1,2,3_더하기/신필.py
@@ -0,0 +1,20 @@
+
+def solution(arr):
+    dp = [0, 1, 2, 4] + [-1]*7
+
+    def dfs(n):
+        if dp[n] != -1:
+            return dp[n]
+        dp[n] = dfs(n-3) + dfs(n-2) + dfs(n-1)
+        return dp[n]
+    return map(lambda x: dfs(x), arr)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        arr.append(int(input()))
+    result = solution(arr)
+    for i in result:
+        print(i)
