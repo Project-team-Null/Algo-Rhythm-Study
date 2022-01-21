88b9ba727ebee390c221ad87bdb795594e89f396
Woohye0n <wh761@g.skku.edu>
2022-01-21 15:49:18 +0900

    [Class3] 2Xn_타일링

--- baekjoon/Class3/2Xn_타일링/우현.py
+++ baekjoon/Class3/2Xn_타일링/우현.py
@@ -6,11 +6,6 @@ if __name__ == "__main__":
     dp = [0] * (n + 4)
     dp[1] = 1
     dp[2] = 2
-    dp[3] = 3
-    dp[4] = 4
-    for i in range(5, n + 1):
-        if i % 2 == 1:
-            dp[i] = dp[i - 1] * 2 - 1
-        else:
-            dp[i] = dp[i - 2] * 5 - 3
-    print(dp[n])
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    print(dp[n] % 10007)
