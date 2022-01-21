60f7a75edbf4002b412aa21d2ede6f76cd465640
Woohye0n <wh761@g.skku.edu>
2022-01-22 00:39:20 +0900

    [Class3] 이중_우선순위_큐

--- baekjoon/Class3/이중_우선순위_큐/우현.py
+++ baekjoon/Class3/이중_우선순위_큐/우현.py
@@ -6,23 +6,38 @@ def solution():
     m = int(read())
     heap_min = []
     heap_max = []
-    for _ in range(m):
+    is_in = [0] * (m + 1)
+
+    for i in range(m):
         cmd, num = read().rstrip().split()
         num = int(num)
-        print(cmd, num)
+
         if cmd == 'I':
-            heapq.heappush(heap_min, num)
-            heapq.heappush(heap_max, -num)
-        elif num == 1:
-            heapq.heappop(heap_max)
+            heapq.heappush(heap_min, (num, i))
+            heapq.heappush(heap_max, (-num, i))
+            is_in[i] = 1
+
         else:
-            heapq.heappop(heap_min)
-    for i in range(len(heap_max)):
-        heap_max[i] *= -1
-    arr = set(heap_max) & set(heap_min)
-    if arr:
-        print("-------", max(arr), min(arr), "-------")
-        return max(arr), min(arr)
+            if num == 1:
+                while heap_max and not is_in[heap_max[0][1]]:
+                    heapq.heappop(heap_max)
+                if heap_max:
+                    is_in[heap_max[0][1]] = 0
+                    heapq.heappop(heap_max)
+            else:
+                while heap_min and not is_in[heap_min[0][1]]:
+                    heapq.heappop(heap_min)
+                if heap_min:
+                    is_in[heap_min[0][1]] = 0
+                    heapq.heappop(heap_min)
+
+    while heap_max and not is_in[heap_max[0][1]]:
+        heapq.heappop(heap_max)
+    while heap_min and not is_in[heap_min[0][1]]:
+        heapq.heappop(heap_min)
+
+    if heap_max and heap_min:
+        return -heap_max[0][0], heap_min[0][0]
     else:
         return 0, 1
 
