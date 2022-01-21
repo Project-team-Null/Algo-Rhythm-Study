0f94e9c8d00c039d75c412cbbba256b7324f8615
sang-jun-kim <59325814+sang-jun-kim@users.noreply.github.com>
2022-01-21 21:04:05 +0900

    [Class 2] 랜선_자르기

--- note/test.py
+++ note/test.py
@@ -1,11 +1,25 @@
 from sys import stdin
 
+def count(lan, num):
+    ret = 0
+    for l in lan:
+        ret += l // num
+    return ret
+
+def bin_search(lan, n, lo, hi):
+    if lo > hi: return hi
+    mid = (lo + hi) // 2
+    cnt = count(lan, mid)
+    if cnt >= n: return bin_search(lan, n, mid+1, hi)
+    elif cnt < n: return bin_search(lan, n, lo, mid-1)
+
 if __name__ == '__main__':
     read = stdin.readline
-    n = int(read().rstrip())
-    arr = [i+1 for i in range(n)]
-    start = 0
-    while arr[start] != arr[-1]:
-        arr.append(arr[start+1])
-        start += 2
-    print(arr[start])
+    k, n = map(int, read().rstrip().split())
+    lan = []
+    for _ in range(k):
+        lan.append(int(read().rstrip()))
+    lan.sort()
+
+    ans = bin_search(lan, n, 1, lan[k-1])
+    print(ans)
