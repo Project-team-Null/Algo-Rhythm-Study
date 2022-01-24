b171c8762f253de449bf51e72428824ab055a75f
lee-3-8 <leesinji8@gmail.com>
2022-01-24 10:22:58 +0900

    [Class 4] N-QUEEN: 수정

--- baekjoon/Class4/N-QUEEN/신필.py
+++ baekjoon/Class4/N-QUEEN/신필.py
@@ -1,10 +1,13 @@
 
 def solution(n):
     queen = [-1]*n
+    result = 0
 
-    def is_able(x):
+    def is_able(x, y):
         for i in range(x):
-            if queen[i] == queen[x] or x - i == abs(queen[x] - queen[i]):
+            dx = x - i
+            dy = abs(y - queen[i])
+            if dy == 0 or dx == dy:
                 return False
         return True
 
@@ -13,13 +16,20 @@ def solution(n):
         if y == n:
             return 1
         else:
-            for i in range(n):
-                queen[y] = i
-                if is_able(y):
+            for i in range(n//2 if y == 0 else n):
+                if is_able(y, i):
+                    queen[y] = i
                     cnt += dfs(y+1)
             return cnt
-
-    return dfs(0)
+    if n == 1:
+        result = 1
+    elif n % 2 == 0:
+        result = 2 * dfs(0)
+    else:
+        result = 2 * dfs(0)
+        queen[0] = n//2
+        result += dfs(1)
+    return result
 
 
 if __name__ == "__main__":
