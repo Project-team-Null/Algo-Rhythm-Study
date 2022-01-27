2d117361b78ac8fa7c605e8ad2ab16046ac5ca90
Woohye0n <wh761@g.skku.edu>
2022-01-27 22:20:57 +0900

    [Class3] 나는야_포켓몬_마스터_이다솜

--- baekjoon/Class3/나는야_포켓몬_마스터_이다솜/우현.py
+++ baekjoon/Class3/나는야_포켓몬_마스터_이다솜/우현.py
@@ -5,12 +5,11 @@ if __name__ == "__main__":
     n, m = map(int, read().split())
     dic = {}
     reverse_dic = {}
-    question = []
     for i in range(1, n + 1):
         temp = read().rstrip()
         dic[i] = temp
         reverse_dic[temp] = i
-    
+
     for i in range(m):
         temp = read().rstrip()
         if temp.isdigit():
