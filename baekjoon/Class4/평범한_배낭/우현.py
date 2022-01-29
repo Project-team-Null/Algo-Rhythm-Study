6fb1237228c14c8f090ba9841b01b4d8554ba3c4
Woohye0n <wh761@g.skku.edu>
2022-01-29 19:20:54 +0900

    [Class4] 평범한_배낭

--- baekjoon/Class4/평범한_배낭/우현.py
+++ baekjoon/Class4/평범한_배낭/우현.py
@@ -1,18 +1,9 @@
-
-def solution(lst, k):
-    dp = [0] * (k + 1)
-    for w, v in lst:
-        for i in range(k, w - 1, -1):
-            dp[i] = max(dp[i], dp[i - w] + v)
-    return dp[-1]
-
-
 if __name__ == "__main__":
     n, k = map(int, input().split())
-    lst = []
     dp = [0] * (k + 1)
-    for i in range(n):
+    for _ in range(n):
         w, v = map(int, input().split())
         if w <= k:
-            lst.append((w, v))
-    print(solution(lst, k))
+            for i in range(k, w - 1, -1):
+                dp[i] = max(dp[i], dp[i - w] + v)
+    print(dp[-1])
