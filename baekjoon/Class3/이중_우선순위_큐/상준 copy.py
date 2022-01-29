c29ec70a4cd1a0594d52cd5982bfb1cb5396cfa5
Woohye0n <wh761@g.skku.edu>
2022-01-29 19:26:17 +0900

    [Class3] 이중_우선순위_큐

--- baekjoon/Class3/이중_우선순위_큐/상준 copy.py
+++ baekjoon/Class3/이중_우선순위_큐/상준 copy.py
@@ -5,41 +5,31 @@ import heapq
 if __name__ == '__main__':
     read = stdin.readline
     t = int(read().rstrip())
-
     for _ in range(t):
         k = int(read().rstrip())
         min_dct, max_dct = defaultdict(int), defaultdict(int)
         min_heap, max_heap = [], []
         size = 0
-
         for _ in range(k):
             cmd, num = read().rstrip().split()
             num = int(num)
-
             if cmd == 'I':
                 heapq.heappush(min_heap, num)
                 heapq.heappush(max_heap, -num)
                 size += 1
-
             elif cmd == 'D' and size != 0:
                 size -= 1
-
                 if num == 1:
-
                     temp = -heapq.heappop(max_heap)
                     while min_dct[temp]:
                         min_dct[temp] -= 1
                         temp = -heapq.heappop(max_heap)
-
                     max_dct[temp] += 1
-
                 elif num == -1:
-
                     temp = heapq.heappop(min_heap)
                     while max_dct[temp]:
                         max_dct[temp] -= 1
                         temp = heapq.heappop(min_heap)
-
                     min_dct[temp] += 1
 
         ans = [0, 0]
