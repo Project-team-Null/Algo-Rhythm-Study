1794ab054c5641f8b18b3f54facc6defb261fd24
lee-3-8 <leesinji8@gmail.com>
2022-01-30 14:01:27 +0900

    [Class 3] 최소_힙:수정

--- baekjoon/Class3/최소_힙/신필.py
+++ baekjoon/Class3/최소_힙/신필.py
@@ -6,23 +6,54 @@ input = sys.stdin.readline
 
 
 class Heap:
-    def __init__(self) -> None:
-        self.que = []
+    length = 0
 
-    def up_heap():
-        pass
+    def __init__(self, is_max) -> None:
+        self.que = [0]
+        self.__is_max = is_max
 
-    def down_heap():
-        pass
+    def compare(self, a, b):
+        return a > b if self.__is_max else a < b
 
-    def heap_push(num):
-        pass
+    def swap(self, a, b):
+        self.que[a], self.que[b] = self.que[b], self.que[a]
 
-    def heap_pop():
-        pass
+    def up_heap(self, index):
+        if 1 >= index:
+            return
+        p = index // 2
+        if self.compare(self.que[index], self.que[p]):
+            self.swap(index, p)
+            self.up_heap(p)
+
+    def down_heap(self, index):
+        if 2 * index > self.length:
+            return
+        l, r = 2 * index, 2 * index + 1
+        if r > self.length:
+            target = l
+        else:
+            target = l if self.compare(self.que[l], self.que[r]) else r
+        if not self.compare(self.que[index], self.que[target]):
+            self.swap(index, target)
+            self.down_heap(target)
+
+    def push(self, num):
+        self.length += 1
+        self.que.append(num)
+        self.up_heap(self.length)
+
+    def pop(self):
+        if self.length == 0:
+            return 0
+        self.swap(self.length, 1)
+        result = self.que.pop()
+        self.length -= 1
+        self.down_heap(1)
+        return result
 
 
-def solution(cmd_list):
+def solution1(cmd_list):
     heap = []
     for cmd in cmd_list:
         if cmd == 0:
@@ -34,9 +65,18 @@ def solution(cmd_list):
             heapq.heappush(heap, cmd)
 
 
+def solution2(cmd_list):
+    heap = Heap(False)
+    for cmd in cmd_list:
+        if cmd == 0:
+            print(heap.pop())
+        else:
+            heap.push(cmd)
+
+
 if __name__ == "__main__":
     n = int(input())
     cmd_list = []
     for _ in range(n):
         cmd_list.append(int(input()))
-    solution(cmd_list)
+    solution2(cmd_list)
