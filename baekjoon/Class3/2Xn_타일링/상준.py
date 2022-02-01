db20304fb2a7fb957636cccfc446b0476fc4872f
sang-jun-kim <sangjun97610@naver.com>
2022-02-01 15:04:06 +0900

    [Class 3] 2Xn_타일링

---
+++ baekjoon/Class3/2Xn_타일링/상준.py
@@ -0,0 +1,9 @@
+from sys import stdin
+
+if __name__ == '__main__':
+    read = stdin.readline
+    n = int(read().rstrip())
+    dp = [0, 1, 2]+ [0]*(n-2)
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    print(dp[n]%10007)
