60b5624f31809e31b513bfb7674b0e2d52846390
Woohye0n <wh761@g.skku.edu>
2022-02-07 19:14:42 +0900

    [Class4] 최단경로

---
+++ baekjoon/Class4/최단경로/우현.py
@@ -0,0 +1,35 @@
+from sys import stdin
+from collections import defaultdict
+import heapq
+
+
+def dijkstra(board, v, k):
+    heap = []
+    distance = [float('inf')] * (v + 1)
+    distance[k] = 0
+    heapq.heappush(heap, (0, k))
+    while heap:
+        dist, current = heapq.heappop(heap)
+        if dist > distance[current]:
+            continue
+        for next_node, w in board[current].items():
+            temp = distance[current] + w
+            if temp < distance[next_node]:
+                distance[next_node] = temp
+                heapq.heappush(heap, (temp, next_node))
+    return distance[1:]
+
+
+if __name__ == "__main__":
+    read = stdin.readline
+    v, e = map(int, read().split())
+    k = int(read())
+    board = {n: defaultdict(lambda: 11) for n in range(1, v + 1)}
+    for _ in range(e):
+        n_from, n_to, w = map(int, read().split())
+        board[n_from][n_to] = min(board[n_from][n_to], w)
+    for i in dijkstra(board, v, k):
+        if i == float('inf'):
+            print("INF")
+        else:
+            print(i)
