2a2f1013b58bf8180f3c1b977968d72b6666f5e7
sang-jun-kim <sangjun97610@naver.com>
2022-02-07 13:13:25 +0900

    [Class 3] 동전_0 and 절댓값_힙

---
+++ baekjoon/Class3/절댓값_힙/상준.py
@@ -0,0 +1,44 @@
+from sys import stdin
+
+def downheap(heap, idx, size):
+    next, left, right = idx, 2*idx, 2*idx+1
+    if left <= size and abs(heap[left]) < abs(heap[next]): next = left
+    if left <= size and abs(heap[left]) == abs(heap[next]) and heap[left] < heap[next]: next = left
+    if right <= size and abs(heap[right]) < abs(heap[next]): next = right
+    if right <= size and abs(heap[right]) == abs(heap[next]) and heap[right] < heap[next]: next = right
+    if next != idx:
+        heap[idx], heap[next] = heap[next], heap[idx]
+        downheap(heap, next, size)
+
+def upheap(heap, idx):
+    if idx == 1: return
+    if abs(heap[idx//2]) > abs(heap[idx]):
+        heap[idx], heap[idx//2] = heap[idx//2], heap[idx]
+        upheap(heap, idx//2)
+    elif abs(heap[idx//2]) == abs(heap[idx]) and heap[idx//2] > heap[idx]:
+        heap[idx], heap[idx//2] = heap[idx//2], heap[idx]
+        upheap(heap, idx//2)
+
+def heapify(heap, size, x):
+    heap[size] = x
+    upheap(heap, size)
+
+if __name__ == '__main__':
+    read = stdin.readline
+    n = int(read().rstrip())
+    heap = [0 for _ in range(100001)]
+    size = 0
+    for _ in range(n):
+        x = read().rstrip()
+        if x == '0':
+            if size == 0:
+                print(0)
+            else:
+                print(heap[1])
+                heap[1], heap[size] = heap[size], heap[1]
+                size -= 1
+                downheap(heap, 1, size)
+        else:
+            size += 1
+            heapify(heap, size, int(x))
+        
