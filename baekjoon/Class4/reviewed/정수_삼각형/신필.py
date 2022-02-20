f7e7f0174138a22ef0c7babcbcdc8ea5bfe9a07e
lee-3-8 <leesinji8@gmail.com>
2022-02-20 15:31:26 +0900

    [reviewed] 2022-02-20

--- baekjoon/Class4/reviewed/정수_삼각형/신필.py
+++ baekjoon/Class4/reviewed/정수_삼각형/신필.py
@@ -4,20 +4,21 @@ input = sys.stdin.readline
 
 def solution(arr, n):
     max_v = 0
-    dp = [[0]*(i+1) for i in range(n)]
+    dp = [[0] * (i + 1) for i in range(n)]
     dp[0][0] = arr[0][0]
 
     def search_tree(i, j):
         if dp[i][j] == 0:
             if j < i:
-                dp[i][j] = search_tree(i-1, j) + arr[i][j]
+                dp[i][j] = search_tree(i - 1, j) + arr[i][j]
             if j > 0:
-                dp[i][j] = max(dp[i][j], search_tree(i-1, j-1) + arr[i][j])
+                dp[i][j] = max(dp[i][j], search_tree(i - 1, j - 1) + arr[i][j])
         return dp[i][j]
-    for j in range(n):
-        search_tree(n-1, j)
 
-    return max(dp[n-1])
+    for j in range(n):
+        search_tree(n - 1, j)
+
+    return max(dp[n - 1])
 
 
 if __name__ == "__main__":
