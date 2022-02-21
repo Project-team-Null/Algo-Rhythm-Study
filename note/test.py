f5e71061a8e774715d69b25fafc7e2548fdc72f4
sang-jun-kim <59325814+sang-jun-kim@users.noreply.github.com>
2022-02-21 18:55:12 +0900

    [Class 4] 트리의_순회

--- note/test.py
+++ note/test.py
@@ -1,8 +1,10 @@
+import enum
 from sys import stdin
 from collections import deque
 from queue import PriorityQueue
 from math import sqrt, ceil
 
 if __name__ == '__main__':
-    for i in range(ceil(sqrt(10))):
-        print(float('inf') + 100)
+    l = [1,2,3,4,5]
+    for i, num in enumerate(l[0:5]):
+        print(num)
